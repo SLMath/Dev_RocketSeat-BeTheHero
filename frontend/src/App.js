e318e1dd866c5caa9d4f513a4eddf4b22bdb85ba
Math <m4th88@gmail.com>
2020-03-29 11:50:50 -0300

    frontend - react basics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,13 @@
-import React, { useState } from 'react';// sempre obrigatório
+import React from 'react';
 
-import Header from './Header';// Sempre primeira letra maiuscula
+import "./global.css";
 
-// Componente do REACT: funcao que retorna HTML (chamado de JSX = JavaScript XML)
+import Logon from './pages/Logon';
+
 function App() {
-  // Estado = variável que representa dado(s) renderizado(s), permitindo atualizaçao automatica da pagina
-  // OBS.: Uma variavel usual não altera o valor exibido
-  const [counter, setCounter] = useState(0); // retorna [valor, funcao de alteracao]
-
-  function increment(){
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header prop="Title">
-        Subtitle<br/>
-        Contador: {counter}
-      </Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Logon />
   );
-} 
+}
 
 export default App; 
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,34 @@
+// Utilidade de usar index.js :: sempre que importa uma pasta, este arquivo é procurado
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi'; // react-icons = Module instalado, fi = feather icons
+
+import "./styles.css";
+
+import logoImg from "../../assets/logo.svg"
+import heroesImg from "../../assets/heroes.png"
+
+function Logon() {
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Logon" />
+
+                <form>
+                    <h1>Faça seu logon</h1>
+
+                    <input placeholder="sua ID" />
+                    <button className="button" type="submit">Entrar</button>
+
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes" />
+        </div>
+    );
+}
+
+export default Logon; 
